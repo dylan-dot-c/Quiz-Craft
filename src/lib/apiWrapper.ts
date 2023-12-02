311093545e90f93c9dc53ed83c731584173e7ce7
Dylan Heslop <heslopd23@gmail.com>
2023-12-01 20:15:26 -0500

    added external url

--- src/lib/apiWrapper.ts
+++ src/lib/apiWrapper.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:5000/api";
+const baseURL = "https://quiz-craft-flask-backend.onrender.com/api";
 
 const apiClientNoAuth = () => {
     return axios.create({
